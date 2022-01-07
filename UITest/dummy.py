c24c2fffe99a7715ec83c193426c0315616e622d
Sophia <44484902+sophierahn@users.noreply.github.com>
2022-01-07 17:11:28 -0500

    Add files via upload
    Trying to get multiprocess up and running

---
+++ UITest/dummy.py
@@ -0,0 +1,23 @@
+import numpy as np
+import math
+import time
+from multiprocessing import Process, Pipe, Queue
+import random
+
+def fillarray(dum_pipe):
+    dumdata = [0]
+    x = 0
+    while x < 1000:
+        data = random.randint(0,9)
+        dumdata.append(data)
+        x = x + 1
+    while x > 1:
+        data2 = dumdata[x]
+        #print(data2)
+        dum_pipe.send(data2)
+        time.sleep(1)
+        x = x - 1
+
+
+#if __name__ == '__main__':
+    #fillarray()
---
+++ UITest/func.py
@@ -0,0 +1,33 @@
+from tkinter import *
+import tkinter as tk
+from time import sleep
+#import RPi.GPIO as GPIO
+import sys
+
+def Missatge(cap,msg):
+    lmsg = len(msg)/50+1
+    tkmsg = tk.Toplevel()
+    tkmsg.wm_title("%s"%str(cap))
+    tkmsg.geometry('500x%s'% str(50*int(lmsg)+50))
+    Lmsg = Label(tkmsg,text="%s"%msg,wraplength=450)
+    #Lmsg.place(x=25,y=10)
+    Lmsg.pack(pady=20)
+    btnok=Button(tkmsg, command=lambda:tkmsg.destroy(), text="Ok", width=6, height=1)
+    btnok.pack()
+    #btnok.place(x=230,y=80)
+
+def isfloat(x):
+    try:
+        a =float(x)
+    except ValueError:
+        return False
+    else:
+        return True
+
+def isint(x):
+    try:
+        a =int(x)
+    except ValueError:
+        return False
+    else:
+        return True

6fd3a732c1bbf97ed094742abcdb1c373e244fd7
bronwynerb <47065369+bronwynerb@users.noreply.github.com>
2022-01-23 19:50:05 -0500

    Did some multiprocess testing that worked???!!!

---
+++ mulitprocessing_testing/mainTest.py
@@ -0,0 +1,58 @@
+#Testing
+from multiprocessing import Process, Pipe, Queue
+#import Queue
+from time import sleep
+from mulitplexer import muliplexer
+from pressure_test import pressureStore
+
+
+powerLevel = [20,30,40,50,60,70,80,90,100,110,120,130,140,150]
+index = 0
+count = 10
+
+#Multiplexer
+q = Queue()
+multi = Process(target= muliplexer, args= (q,))
+multi.start()
+#q.put_nowait(11)
+
+#Power Supply
+psen_pipe, mainp_pipe = Pipe()
+psen_script = Process(target= pressureStore, args= (psen_pipe,q))
+psen_script.start()  
+#mainp_pipe.send(False)
+
+pressure = 22
+
+while count > 0:
+    while mainp_pipe.poll():
+        pressure = mainp_pipe.recv()
+    print("pressure =", pressure)
+    q.put_nowait((1,powerLevel[index]))
+
+    if pressure%5 == 0:
+        #mainp_pipe.send(True)
+        q.put_nowait((2,True))
+        sleep(1)
+        multi.terminate()
+        psen_script.terminate()
+        break
+
+    index += 1
+    count -= 1
+    sleep(1)
+
+q.put_nowait((2,True))
+sleep(1)
+multi.terminate()
+psen_script.terminate()
+
+
+
+
+
+
+
+
+
+
---
+++ mulitprocessing_testing/pressure_test.py
@@ -0,0 +1,32 @@
+import random
+from time import sleep
+
+def pressureStore(psen_pipe,q):
+    shutoff = False
+    dataCheck = []
+    queueDump = []
+    pressure = 11
+
+    while not shutoff:
+        while not q.empty():
+            try:
+                queueDump.append(q.get_nowait())
+            except:
+                pass
+        for i in queueDump:
+            if i[0] == 0:
+                pressure = i[1]
+                psen_pipe.send(pressure)
+                dataCheck.append(pressure)
+            if i[0] == 1:
+                q.put_nowait((1,i[1]))
+            if i[0] == 2:
+                shutoff = i[1]
+                #print("pressure Shutting off")
+                #print("Pressure:",dataCheck)
+                q.put_nowait((2,shutoff))
+        queueDump = []
+    print("Pressure Closed")
+    print("Pressure:",dataCheck[-10:-1])
+    psen_pipe.close()
+           

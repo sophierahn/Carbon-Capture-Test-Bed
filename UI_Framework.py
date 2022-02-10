96d91c8aa4622c14227cfeb1f644dfe4d172df6d
bronwynerb <bronwynerb@gmail.com>
2022-02-09 23:46:38 -0500

    added Graphing, not working yet

--- UI_Framework.py
+++ UI_Framework.py
@@ -30,16 +30,16 @@ if not mac:
     from detect_bright_spots import start_imageCapture
     from mulitplexer import muliplexer
     from power_sensor import power_log
-    
-    
+
+
     #If running on pi, this is to mount CSV flashdrive, giving us a folder to send data to
     #sdc1 is a potential flashdrive name, may not be true for the pi
     #Check and adjust using lsblk on the command line, it should be the same name every time
     #I'd love to make this more dynamic than a hardcoded file name but idk how to do that rn
-    #The name of the device should be sdXY where X is any letter and Y any digit 
+    #The name of the device should be sdXY where X is any letter and Y any digit
     #This doesn't need to be done in a specific process bc it's interacting with the OS
-    #subprocess.run(["udisksctl",  "mount",  "-b",  "/dev/sdc1"]) 
-    
+    #subprocess.run(["udisksctl",  "mount",  "-b",  "/dev/sdc1"])
+
 
 ImageFile.LOAD_TRUNCATED_IMAGES = True
 
@@ -49,11 +49,12 @@ EntTime = [None]*1
 EntPump = [None]*1
 
 estop = False
+graph = False
 countdown = None
 debug = False
 
 class controls:
-    global radioVar, var, estop, countdown, testDefault
+    global radioVar, var, estop, countdown, testDefault, graph
     testDefault = [0,0,0,3]
 
     def __init__(self, master):
@@ -79,25 +80,25 @@ class controls:
         #### Data variables ####
         self.pressure = float(0)
         self.saltArea = float(0)
-        # self.canvas.create_line(1,1,1,255, width = 3, fill="#666666")    
+        # self.canvas.create_line(1,1,1,255, width = 3, fill="#666666")
 
         #Master Title
         self.lblTitle = Label(master, text="MEA Cell Controls", font=("Calibri",text+14))
         self.lblTitle.place(x=400,y=10)
 
-        
+
     #### Settings Titles ####
-        self.TitlPump = Label(master, text="Pump Controls", font=("Calibri",text+8))
-        self.TitlPump.place(x=setX,y=setY)
+        #self.TitlPump = Label(master, text="Pump Controls", font=("Calibri",text+8))
+        #self.TitlPump.place(x=setX,y=setY)
         self.TitlFlow = Label(master, text="Flow Controller", font=("Calibri",text+8))
         self.TitlFlow.place(x=setX,y=setY+100)
         self.TitlPower = Label(master, text="Power Supply", font=("Calibri",text+8))
         self.TitlPower.place(x=setX,y=setY+200)
         #Countdown Timer
         self.LblTimer = Label(master, text="Remaining Time:", font=("Calibri",text+6))
-        self.LblTimer.place(x=setX+100,y=setY-50)
+        self.LblTimer.place(x=setX,y=setY-50)
         self.LblCountdown = Label(master, text="", font=("Calibri",text+6))
-        self.LblCountdown.place(x=setX+60,y=setY-50)
+        self.LblCountdown.place(x=setX+140,y=setY-50)
 
     #### Settings Controls ####
         def only_numbers(char):
@@ -106,14 +107,14 @@ class controls:
             else:
                 return False
         validation = root.register(only_numbers)
-        
+
 
         #Pump controls
-        EntPump[0] = Entry(master, width=5, justify=RIGHT, validate="key", validatecommand=(validation, '%S'))
-        self.LblPump = Label(master, text="Flow Rate:", font=("Calibri",text+4))
-        EntPump[0].place(x=entX,y=setY+30)
-        EntPump[0].insert(0,testDefault[0])#Set Default
-        self.LblPump.place(x=setX,y=setY+30)
+        # EntPump[0] = Entry(master, width=5, justify=RIGHT, validate="key", validatecommand=(validation, '%S'))
+        # self.LblPump = Label(master, text="Flow Rate:", font=("Calibri",text+4))
+        # EntPump[0].place(x=entX,y=setY+30)
+        # EntPump[0].insert(0,testDefault[0])#Set Default
+        # self.LblPump.place(x=setX,y=setY+30)
 
         #Flow Controller controls
         EntFlow[0] = Entry(master, width=5, justify=RIGHT, validate="key", validatecommand=(validation, '%S'))
@@ -142,10 +143,11 @@ class controls:
         EntTime[0].place(x=entX,y=setY+340)
         EntTime[0].insert(0,testDefault[3])#Set Default
         self.LblTime.place(x=setX-20,y=setY+340)
-        
+
         datax = 600
         datay = 80
     #### Data ####
+        #pressure
         self.dataHeading = Label(master, text="Live Data", font=("Calibri",text+8))
         self.dataHeading.place(x=datax+100,y=datay)
         self.psen0 = Label(master, text="Inlet 1: 0kPa", font=("Calibri",text+6))
@@ -156,13 +158,13 @@ class controls:
         self.psen2.place(x=datax,y=datay+70)
         self.psen3 = Label(master, text="Outlet 2: 0kPa", font=("Calibri",text+6))
         self.psen3.place(x=datax+170,y=datay+70)
-        
+        #power
         self.power0 = Label(master, text="Current: 0 mA", font=("Calibri",text+6))
-        self.power0.place(x=datax-10,y=datay+105)
+        self.power0.place(x=datax-50,y=datay+105)
         self.power1 = Label(master, text="Voltage: 0 V", font=("Calibri",text+6))
         self.power1.place(x=datax+110,y=datay+105)
         self.power2 = Label(master, text="Power: 0 mW", font=("Calibri",text+6))
-        self.power2.place(x=datax+220,y=datay+105)
+        self.power2.place(x=datax+250,y=datay+105)
 
         salty = datay + 160
         self.imageHeading = Label(master, text="Salt Idenification", font=("Calibri",text+8))
@@ -189,7 +191,7 @@ class controls:
         self.BtnPurge.place(x=btnX+130,y=btnY+20)
         self.BtnDefault = Button(master, text="Save Test Default", command=lambda: self.saveTest())
         self.BtnDefault.place(x=20,y=20)
-        
+
         self.BtnClose = Button(master, text="Close", command=lambda: self.close())
         self.BtnClose.place(x=10,y=580)
 
@@ -211,7 +213,7 @@ class controls:
     def saveTest(self):
         global testDefault
         try:
-            testDefault[0] = float(EntPump[0].get())
+            #testDefault[0] = float(EntPump[0].get())
             testDefault[1] = float(EntFlow[0].get())
             testDefault[2] = float(EntPower[0].get())
             testDefault[3] = float(EntTime[0].get())
@@ -227,32 +229,32 @@ class controls:
         if 'psen_script' in globals():
             psen_script.terminate()
         if 'image_script' in globals():
-            image_script.terminate()   
-        if 'multi' in globals(): 
+            image_script.terminate()
+        if 'multi' in globals():
             multi.terminate()
         if 'power_script' in globals():
-            power_script.terminate()     
+            power_script.terminate()
 
     #closes window
     def close(self):
         if debug:
-            print("closing")      
+            print("closing")
         self.killProcesses()
         sys.exit(0)
-    
+
     ##Estop Function //Need to add a reset fuctionality to change estop back to false so tests can be restarted
-    def stopTest(self): 
+    def stopTest(self):
         global estop
         if not estop:
             self.killProcesses()
-            estop = True   
+            estop = True
             self.BtnCancel.config(text="Reset", bg='#FF0000', activebackground='#FF0000')
             EntFlow[0].delete(0,'end')
-            EntPump[0].delete(0,'end')
+            #EntPump[0].delete(0,'end')
             EntPower[0].delete(0,'end')
             EntTime[0].delete(0,'end')
             EntFlow[0].insert(0,str(testDefault[0]))
-            EntPump[0].insert(0,testDefault[1])
+            #EntPump[0].insert(0,testDefault[1])
             EntPower[0].insert(0,testDefault[2])
             EntTime[0].insert(0,testDefault[3])
         else:
@@ -268,12 +270,12 @@ class controls:
             print("validating")
         try:
             gasFlow = float(EntFlow[0].get())
-            liquidFlow = float(EntPump[0].get())
+            #liquidFlow = float(EntPump[0].get())
             testMin = float(EntTime[0].get())
             powerVC = float(EntPower[0].get())
         except:
            func.Missatge("Warning","Numerical Entry Invalid")
-        
+
         #Multiplexer
         q = Queue()
         multi = Process(target= muliplexer, args= (q,))
@@ -282,17 +284,17 @@ class controls:
         #Initiallize pressure sensor process
         psen_pipe, mainp_pipe = Pipe()
         psen_script = Process(target= start_psensor, args= (psen_pipe,q,))
-        psen_script.start()  
+        psen_script.start()
 
         #power sensor process
         power_pipe, mainpower_pipe = Pipe()
         power_script = Process(target= power_log, args= (power_pipe,q,))
-        power_script.start()  
+        power_script.start()
 
         #Initiallize Image Capture Process
         # image_pipe, maini_pipe = Pipe()
         # image_script = Process(target= start_imageCapture, args= (image_pipe,))
-        # image_script.start()  
+        # image_script.start()
         # maini_pipe.send(False)
 
         #Call repeating functions
@@ -320,9 +322,8 @@ class controls:
         global estop
         if debug:
             print("Pressure Sensor")
-            print("the main pipe poll comes back: ",mainp_pipe.poll()) 
+            print("the main pipe poll comes back: ",mainp_pipe.poll())
         if not estop:
-            pressureArray =[]
             pressureList = [0]*4
             while mainp_pipe.poll():
                 pressureList= mainp_pipe.recv()
@@ -332,23 +333,8 @@ class controls:
             self.pressure_2 = round(pressureList[2],3)
             self.pressure_3 = round(pressureList[3],3)
 
-            # plt.ion()
-            # plt.clf()
-            # index = range(0,testSec)
-            # pressureArray.append(self.pressure_0)
-            # l = len(pressureArray)
-            # plt.plot(index[0:l],pressureArray)
-            # if index[-1]>10:
-            #     pressureArray.pop(0)
-            #     index.pop(0)
-            #     #print(x[-12:-1])
-            #     #print(index[-12:-1])
-            #     plt.xlim([index[-10], index[-1]])
-            # else:
-            #     plt.xlim([0, 10])
-            # plt.ylim([0, 100])
-            # plt.show
-            
+            self.graphing(self.pressure_0,self.pressure_1,self.pressure_2,self.pressure_3, testSec)
+
             self.psen0.config(text = "Inlet 1: "+str(self.pressure_0)+"kPa")
             self.psen1.config(text = "Outlet 1: "+str(self.pressure_1)+"kPa")
             self.psen2.config(text = "Inlet 2: "+str(self.pressure_2)+"kPa")
@@ -361,7 +347,7 @@ class controls:
         global estop
         if debug:
             print("Pressure Sensor")
-            print("the main pipe poll comes back: ",mainpower_pipe.poll()) 
+            print("the main pipe poll comes back: ",mainpower_pipe.poll())
         if not estop:
             powerList = [0]*3
             while mainpower_pipe.poll():
@@ -369,8 +355,7 @@ class controls:
             self.power_0 = round(powerList[0],3)
             self.power_1 = round(powerList[1],3)
             self.power_2 = round(powerList[2],3)
-            
-           
+
             self.power0.config(text = "Current: "+str(self.power_0)+" mA")
             self.power1.config(text = "Voltage: "+str(self.power_1)+" V")
             self.power2.config(text = "Power: "+str(self.power_2)+" mW")
@@ -382,7 +367,7 @@ class controls:
         global estop
         if debug:
             print("Image Capture Salt Area")
-            print("the main pipe poll comes back: ",maini_pipe.poll())         
+            print("the main pipe poll comes back: ",maini_pipe.poll())
         if not estop:
             while maini_pipe.poll():
                 self.saltArea = round(maini_pipe.recv(),3)
@@ -399,8 +384,42 @@ class controls:
             image_capture = root.after(1000, lambda: self.image_capture(image_pipe, maini_pipe))
         else:
             root.after_cancel(self.image_capture)
-        
+
+    def graphing(self, press0, press1, press2, press3, testSec):
+        global graph, estop
+        while graph and not estop:
+            press_0 = []
+            press_1 = []
+            press_2 = []
+            press_3 = []
+
+            plt.ion()
+            plt.clf()
+            index = list(range(0,testSec+1))
+            press_0.append(press0)
+            press_1.append(press1)
+            press_2.append(press2)
+            press_3.append(press3)
+
+            l = len(press_0)
+            plt.plot(index[0:l],press_0)
+            plt.plot(index[0:l],press_1)
+            plt.plot(index[0:l],press_2)
+            plt.plot(index[0:l],press_3)
+            scroll = index[0:l]
+            if scroll[-1]>10:
+                press_0.pop(0)
+                press_1.pop(1)
+                press_2.pop(2)
+                press_3.pop(3)
+                index.pop(0)
+                plt.xlim([index[-10], index[-1]])
+            else:
+                plt.xlim([0, 10])
+            plt.ylim([0, 100])
+            plt.show
+            plt.pause(0.05)
 
 root = Tk()
-my_gui = controls(root)  
+my_gui = controls(root)
 root.mainloop()
